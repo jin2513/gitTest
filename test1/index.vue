f183a43f3734f0643a9bdad1a89a58dcdf5cb0c8
leeyj9 <leeyj9@brandi.co.kr>
2021-01-25 13:45:13 +0900

    test1 폴더 추가

---
+++ test1/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="container" class="login-page">
+    <div class="login">
+      <div class="section">
+        <div class="tit">
+        </div>
+        <div class="fields">
+          <ul>
+            <li><input ref="userId" type="text" v-model.trim="userId" class="comText" placeholder="아이디를 입력해주세요" @keyup.enter="login" /></li>
+            <li><input ref="userPw" type="password" v-model.trim="userPw" class="comText" placeholder="비밀번호를 입력해주세요" autocomplete="off" @keyup.enter="login" /></li>
+          </ul>
+          <button type="submit" class="btLogin" @click.prevent="login">LOGIN</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
