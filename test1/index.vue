ce915f0250e5a3d36077b8ddb9985d3f9b47053b
leeyj9 <leeyj9@brandi.co.kr>
2021-01-26 09:57:45 +0900

    li 추가

--- test1/index.vue
+++ test1/index.vue
@@ -9,6 +9,7 @@
             <li><input ref="userId" type="text" v-model.trim="userId" class="comText" placeholder="아이디를 입력해주세요" @keyup.enter="login" /></li>
             <li><input ref="userPw" type="password" v-model.trim="userPw" class="comText" placeholder="비밀번호를 입력해주세요" autocomplete="off" @keyup.enter="login" /></li>
             <li>test</li>
+            <li>aaaaa</li>
           </ul>
           <button type="submit" class="btLogin" @click.prevent="login">LOGIN</button>
         </div>
