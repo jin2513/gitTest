181334f5c68b7cc643e3e222427bcc364b1b7a9a
leeyj9 <leeyj9@brandi.co.kr>
2021-01-26 09:22:04 +0900

    추가

--- test1/index.vue
+++ test1/index.vue
@@ -1,5 +1,3 @@
-test
-
 <template>
   <div id="container" class="login-page">
     <div class="login">
@@ -10,6 +8,7 @@ test
           <ul>
             <li><input ref="userId" type="text" v-model.trim="userId" class="comText" placeholder="아이디를 입력해주세요" @keyup.enter="login" /></li>
             <li><input ref="userPw" type="password" v-model.trim="userPw" class="comText" placeholder="비밀번호를 입력해주세요" autocomplete="off" @keyup.enter="login" /></li>
+            <li>test</li>
           </ul>
           <button type="submit" class="btLogin" @click.prevent="login">LOGIN</button>
         </div>
